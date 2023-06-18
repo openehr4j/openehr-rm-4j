06f1aa6438f8521b5d2acd19e85a9b1339313dcd
Jan Mewes <jan.mewes@ks-plus.org>
2023-06-18 12:40:16 +0200

    ci: auto-update version numbers

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NEW_VERSION=$1
 
 NEW_VERSION_GRADLE="ext['openehrRmVersion'] = '$NEW_VERSION'"
-NEW_VERSION_MAVEN="        <openehr.rm.version>$NEW_VERSION</openehr.rm.version>"
+NEW_VERSION_MAVEN="        <openehr.rm.version>$NEW_VERSION<\/openehr.rm.version>"
 
 perl -pi -e "s/ext\[.*openehrRmVersion.*/$NEW_VERSION_GRADLE/g" $SCRIPT_DIR/../../README.md
-perl -pi -e "s/.*<openehr.rm.version>.*/$NEW_VERSION_MAVEN/g" $SCRIPT_DIR/../../README.md
+perl -pi -e "s/.*<openehr\.rm\.version>.*/$NEW_VERSION_MAVEN/g" $SCRIPT_DIR/../../README.md
