7ca9e10cb0db3c0a7a6e030b6f2f5f6575c9b0f0
Jan Mewes <jan.mewes@ks-plus.org>
2023-07-08 16:02:06 +0200

    fix: manually adapt generated code (#11)

--- openehr-rm-common/src/main/java/org/openehr/rm/common/change_control/OriginalVersion.java
+++ openehr-rm-common/src/main/java/org/openehr/rm/common/change_control/OriginalVersion.java
@@ -3,12 +3,13 @@ package org.openehr.rm.common.change_control;
 import java.util.List;
 
 import org.openehr.base.base_types.identification.ObjectVersionId;
+import org.openehr.rm.common.generic.Attestation;
 import org.openehr.rm.data_types.text.DvCodedText;
 
 /**
  * A Version containing locally created content and optional attestations.
  */
-public interface OriginalVersion extends Version {
+public interface OriginalVersion<T> extends Version<T> {
   /**
    * Stored version of inheritance precursor.
    */
@@ -22,7 +23,7 @@ public interface OriginalVersion extends Version {
   /**
    * Identifiers of other versions whose content was merged into this version, if any.
    */
-  List getOtherInputVersionUids();
+  List<ObjectVersionId> getOtherInputVersionUids();
 
   /**
    * Lifecycle state of the content item in this version; coded by openEHR vocabulary `version lifecycle state`.
@@ -32,12 +33,12 @@ public interface OriginalVersion extends Version {
   /**
    * Set of attestations relating to this version.
    */
-  List getAttestations();
+  List<Attestation> getAttestations();
 
   /**
    * Data content of this Version.
    */
-  Object getData();
+  T getData();
 
   /**
    * True if this Version was created from more than just the preceding (checked out) version.
